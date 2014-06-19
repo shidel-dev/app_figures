580fe644c696c70b3e89e73ec4c916b92af51e43
Shidel <joe@mytastebud.com>
2014-06-19 10:17:03 -0500

    sets up spec helper and adds passing test

--- Gemfile
+++ Gemfile
@@ -22,4 +22,5 @@ group :test do
   gem 'minitest', require: false
   gem 'minitest-spec-context', require: false
   gem 'minitest-reporters', require: false
+  gem 'mocha', require: false
 end
---
+++ spec/appfigures/test_spec.rb
@@ -0,0 +1,11 @@
+# encoding: utf-8
+
+require 'spec_helper'
+
+describe Appfigures do
+  it 'can run tests' do
+    app = {}
+    app['name'] = 'Appfigures'
+    app['name'].must_equal 'Appfigures'
+  end
+end
