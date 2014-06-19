4bb18f3cb90c70f5bf83dd536608d359d35ebac8
Shidel <joe@mytastebud.com>
2014-06-19 16:02:46 -0500

    removes api version and auth stratgy from specs

--- spec/appfigures/appfigure_spec.rb
+++ spec/appfigures/appfigure_spec.rb
@@ -7,8 +7,6 @@ describe Appfigures do
 
     before do
       @appfigures = Appfigures.new do |config|
-        config.api_version = '2.0'
-        config.auth_stratgy = 'Basic'
         config.client_key = 'your client key'
         config.credentials = 'base 64 encoded username:password'
       end
@@ -18,14 +16,6 @@ describe Appfigures do
       Appfigures.new do |config|
         config.must_be_instance_of Appfigures::Client
       end
-    end
-
-    it 'can configure an api version' do
-      @appfigures.api_version.must_equal '2.0'
-    end
-
-    it 'can configure an auth stratgy' do
-      @appfigures.auth_stratgy.must_equal 'Basic'
     end
 
     it 'can configure a client key' do
