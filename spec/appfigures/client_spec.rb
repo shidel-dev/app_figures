4bab56f91a9eb43f30fba248c9128a05ba06f49b
Shidel <joe@mytastebud.com>
2014-06-19 20:47:00 -0500

    adds spec for configuring with env vars

--- spec/appfigures/client_spec.rb
+++ spec/appfigures/client_spec.rb
@@ -10,5 +10,13 @@ describe AppFigures::Client do
       client.client_key.wont_be_nil
       client.credentials.wont_be_nil
     end
+
+    it 'defualts to env vars if no block or options hash given' do
+      ENV['client_key'] = 'abcd'
+      ENV['credentials'] = '1234'
+      client = AppFigures::Client.new()
+      client.client_key.wont_be_nil
+      client.credentials.wont_be_nil
+    end
   end
 end
