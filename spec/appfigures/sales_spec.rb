350d3305f0a410187102d73a0b6b2fd254fb12b2
Shidel <joe@mytastebud.com>
2014-06-20 10:00:35 -0500

    naming mistake

--- spec/appfigures/sales_spec.rb
+++ spec/appfigures/sales_spec.rb
@@ -4,8 +4,6 @@ require 'spec_helper'
 
 describe AppFigures::Client do
   context '.sales' do
-    it 'includes httparty' do
-      AppFigures::Client.included_modules.must_include HTTParty
-    end
+
   end
 end
