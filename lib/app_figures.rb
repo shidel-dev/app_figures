5db376a73982c23a7acd26a6b20ade123ccf82b6
Shidel <joe@mytastebud.com>
2014-06-19 20:32:30 -0500

    adds the ability to pass an options hash or use block for config

--- lib/app_figures.rb
+++ lib/app_figures.rb
@@ -2,9 +2,7 @@ require 'app_figures/version'
 require 'app_figures/client'
 
 module AppFigures
-  def self.configure
-    client = Client.new
-    yield client
-    client
+  def self.configure(&block)
+    Client.new({}, &block)
   end
 end
--- lib/app_figures/client.rb
+++ lib/app_figures/client.rb
@@ -1,6 +1,16 @@
 module AppFigures
+
   class Client
     attr_accessor :client_key, :credentials
 
+    def initialize(options = {}, &block)
+
+      if block_given?
+        block.call(self)
+      else
+        @client_key = options[:client_key]
+        @credentials = options[:credentials]
+      end
+    end
   end
 end
