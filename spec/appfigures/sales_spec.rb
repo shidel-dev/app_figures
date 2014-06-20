6b4c789566f4f8ab9a8a82588980b810649e7db3
Shidel <joe@mytastebud.com>
2014-06-20 11:55:59 -0500

    indents

--- spec/appfigures/sales_spec.rb
+++ spec/appfigures/sales_spec.rb
@@ -8,32 +8,32 @@ describe AppFigures::Client do
       @basic_client = AppFigures::Client.new({client_key: 'ccfihtger13jfhels', credentials: 'Basic YSHFH123HTJDOF=='})
 
       @basic_client.class.expects(:get)
-      .with('/v2/sales',
-        {headers: {'X-Client-Key' => @basic_client.client_key, 'Authorization' => @basic_client.credentials}})
-      .returns({"downloads"=>1000,
-                "updates"=>2000,
-                "returns"=>0,
-                "net_downloads"=>3000,
-                "promos"=>0,
-                "revenue"=>"0.00",
-                "edu_downloads"=>0,
-                "gifts"=>0,
-                "gift_redemptions"=>0})
+        .with('/v2/sales',
+          {headers: {'X-Client-Key' => @basic_client.client_key, 'Authorization' => @basic_client.credentials}})
+        .returns({"downloads"=>1000,
+                  "updates"=>2000,
+                  "returns"=>0,
+                  "net_downloads"=>3000,
+                  "promos"=>0,
+                  "revenue"=>"0.00",
+                  "edu_downloads"=>0,
+                  "gifts"=>0,
+                  "gift_redemptions"=>0})
 
       @query_client = AppFigures::Client.new({client_key: 'ccfihtger13jfhels', credentials: 'Basic YSHFH123HTJDOF=='})
 
       @query_client.class.expects(:get)
-      .with('/v2/sales',
-        {headers: {'X-Client-Key' => @query_client.client_key, 'Authorization' => @query_client.credentials}, query: {start_date: '2014-01-01'}})
-      .returns({"downloads"=>3000,
-                "updates"=>6000,
-                "returns"=>0,
-                "net_downloads"=>3000,
-                "promos"=>0,
-                "revenue"=>"0.00",
-                "edu_downloads"=>0,
-                "gifts"=>0,
-                "gift_redemptions"=>0})
+        .with('/v2/sales',
+          {headers: {'X-Client-Key' => @query_client.client_key, 'Authorization' => @query_client.credentials}, query: {start_date: '2014-01-01'}})
+        .returns({"downloads"=>3000,
+                  "updates"=>6000,
+                  "returns"=>0,
+                  "net_downloads"=>3000,
+                  "promos"=>0,
+                  "revenue"=>"0.00",
+                  "edu_downloads"=>0,
+                  "gifts"=>0,
+                  "gift_redemptions"=>0})
     end
 
 
