f27bf27a53a285dc1105e13440e40a89ce92cdf7
Shidel <joe@mytastebud.com>
2014-06-20 14:07:19 -0500

    w/s
    [ci skip]

--- spec/appfigures/client_spec.rb
+++ spec/appfigures/client_spec.rb
@@ -17,17 +17,16 @@ describe AppFigures::Client do
 
     it 'takes an option hash' do
       client = AppFigures::Client.new({client_key: 'my client key', credentials: "my credentials"})
+
       client.client_key.wont_be_nil
       client.credentials.wont_be_nil
     end
 
     it 'defualts to env vars if no block or options hash is given' do
-
       client = AppFigures::Client.new()
 
       client.client_key.wont_be_nil
       client.credentials.wont_be_nil
-
     end
   end
 
