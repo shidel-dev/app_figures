3d0381e0331f6d82083edb547fda895415518d0a
Eric Marden <eric@xentek.net>
2014-06-20 18:37:31 -0500

    cleans up specs a bit

--- spec/app_figures/client_spec.rb
+++ spec/app_figures/client_spec.rb
@@ -17,30 +17,25 @@ describe AppFigures::Client do
 
     it 'takes an option hash' do
       client = AppFigures::Client.new({client_key: 'my client key', credentials: "my credentials"})
-
       client.client_key.wont_be_nil
       client.credentials.wont_be_nil
     end
 
     it 'defualts to env vars if no block or options hash is given' do
-      client = AppFigures::Client.new()
-
+      client = AppFigures::Client.new
       client.client_key.wont_be_nil
       client.credentials.wont_be_nil
     end
   end
 
   context '.new without env vars' do
-
     it 'throws error if credentials or client key is nil' do
       Proc.new{ AppFigures::Client.new }.must_raise ArgumentError
     end
-
   end
 
   it 'has a base url' do
-    client = AppFigures::Client.new({client_key: 'my client key', credentials: "my credentials"})
-
+    client = AppFigures::Client.new(client_key: 'my client key', credentials: "my credentials")
     client.class.base_uri.must_equal 'https://api.appfigures.com/v2'
   end
 end
--- spec/app_figures/sales_spec.rb
+++ spec/app_figures/sales_spec.rb
@@ -5,68 +5,20 @@ require 'spec_helper'
 describe AppFigures::Client do
   context '.sales' do
     before do
-      @basic_client = AppFigures::Client.new({client_key: 'exampleclientkey', credentials: 'Basic examplebase64encodedstring'})
-
-      @basic_client.class.expects(:get)
-        .with('/sales',
-          {headers: {'X-Client-Key' => @basic_client.client_key, 'Authorization' => @basic_client.credentials}})
-        .returns({
-          "downloads"=>1000,
-          "updates"=>2000,
-          "returns"=>0,
-          "net_downloads"=>3000,
-          "promos"=>0,
-          "revenue"=>"0.00",
-          "edu_downloads"=>0,
-          "gifts"=>0,
-          "gift_redemptions"=>0
-        })
-
-      @query_client = AppFigures::Client.new({client_key: 'exampleclientkey', credentials: 'Basic examplebase64encodedstring'})
-
-      @query_client.class.expects(:get)
-        .with('/sales',
-          {headers: {'X-Client-Key' => @query_client.client_key, 'Authorization' => @query_client.credentials}, query: {start_date: '2014-01-01'}})
-        .returns({
-          "downloads"=>3000,
-          "updates"=>6000,
-          "returns"=>0,
-          "net_downloads"=>3000,
-          "promos"=>0,
-          "revenue"=>"0.00",
-          "edu_downloads"=>0,
-          "gifts"=>0,
-          "gift_redemptions"=>0
-        })
+      @client = AppFigures::Client.new({client_key: 'exampleclientkey', credentials: 'examplebase64encodedstring'})
+      @headers = { 'X-Client-Key' => @client.client_key, 'Authorization' => "Basic #{@client.credentials}" }
+    end
+    
+    it "requests results" do
+      options = { query: {}, headers: @headers }
+      @client.class.expects(:get).with('/sales', options)      
+      @client.sales
     end
 
-
-    it 'uses HTTParty::get' do
-      @basic_client.sales().must_equal({
-        "downloads"=>1000,
-        "updates"=>2000,
-        "returns"=>0,
-        "net_downloads"=>3000,
-        "promos"=>0,
-        "revenue"=>"0.00",
-        "edu_downloads"=>0,
-        "gifts"=>0,
-        "gift_redemptions"=>0
-      })
-    end
-
-    it 'accepts query params' do
-      @query_client.sales({start_date: '2014-01-01'}).must_equal({
-        "downloads"=>3000,
-        "updates"=>6000,
-        "returns"=>0,
-        "net_downloads"=>3000,
-        "promos"=>0,
-        "revenue"=>"0.00",
-        "edu_downloads"=>0,
-        "gifts"=>0,
-        "gift_redemptions"=>0
-      })
+    it "requests results with a start date" do
+      options = { query: { start_date: '2014-01-01' }, headers: @headers }
+      @client.class.expects(:get).with('/sales', options)
+      @client.sales(start_date: '2014-01-01')
     end
   end
 end
