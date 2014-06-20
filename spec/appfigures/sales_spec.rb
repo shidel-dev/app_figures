dca394dcf76c48b993e9fb4e664bc61862f2ed90
Shidel <joe@mytastebud.com>
2014-06-20 11:39:12 -0500

    adds test for query params

--- spec/appfigures/sales_spec.rb
+++ spec/appfigures/sales_spec.rb
@@ -5,11 +5,11 @@ require 'spec_helper'
 describe AppFigures::Client do
   context '.sales' do
     before do
-      @client = AppFigures::Client.new({client_key: 'ccfihtger13jfhels', credentials: 'Basic YSHFH123HTJDOF=='})
+      @basic_client = AppFigures::Client.new({client_key: 'ccfihtger13jfhels', credentials: 'Basic YSHFH123HTJDOF=='})
 
-      @client.class.expects(:get)
+      @basic_client.class.expects(:get)
       .with('/v2/sales',
-        headers: {'X-Client-Key' => @client.client_key, 'Authorization' => @client.credentials})
+        {headers: {'X-Client-Key' => @basic_client.client_key, 'Authorization' => @basic_client.credentials}})
       .returns({"downloads"=>1000,
                 "updates"=>2000,
                 "returns"=>0,
@@ -20,11 +20,25 @@ describe AppFigures::Client do
                 "gifts"=>0,
                 "gift_redemptions"=>0})
 
+      @query_client = AppFigures::Client.new({client_key: 'ccfihtger13jfhels', credentials: 'Basic YSHFH123HTJDOF=='})
+
+      @query_client.class.expects(:get)
+      .with('/v2/sales',
+        {headers: {'X-Client-Key' => @query_client.client_key, 'Authorization' => @query_client.credentials}, query: {start_date: '2014-01-01'}})
+      .returns({"downloads"=>3000,
+                "updates"=>6000,
+                "returns"=>0,
+                "net_downloads"=>3000,
+                "promos"=>0,
+                "revenue"=>"0.00",
+                "edu_downloads"=>0,
+                "gifts"=>0,
+                "gift_redemptions"=>0})
     end
 
 
     it 'uses HTTParty::get' do
-      @client.sales().must_equal({"downloads"=>1000,
+      @basic_client.sales().must_equal({"downloads"=>1000,
                 "updates"=>2000,
                 "returns"=>0,
                 "net_downloads"=>3000,
@@ -33,6 +47,18 @@ describe AppFigures::Client do
                 "edu_downloads"=>0,
                 "gifts"=>0,
                 "gift_redemptions"=>0})
+    end
+
+    it 'accepts query params' do
+      @query_client.sales({start_date: '2014-01-01'}).must_equal({"downloads"=>3000,
+          "updates"=>6000,
+          "returns"=>0,
+          "net_downloads"=>3000,
+          "promos"=>0,
+          "revenue"=>"0.00",
+          "edu_downloads"=>0,
+          "gifts"=>0,
+          "gift_redemptions"=>0})
     end
   end
 end
