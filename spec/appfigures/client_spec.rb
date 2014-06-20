417af2d0b53f53d7fe2c7bfe07b965380ba8dc8a
Shidel <joe@mytastebud.com>
2014-06-20 12:10:17 -0500

    ENV vars configured in a before block

--- spec/appfigures/client_spec.rb
+++ spec/appfigures/client_spec.rb
@@ -5,6 +5,11 @@ require 'spec_helper'
 describe AppFigures::Client do
   context '.new' do
 
+    before do
+      ENV['client_key'] = 'abcd'
+      ENV['credentials'] = '1234'
+    end
+
     it 'takes an option hash' do
       client = AppFigures::Client.new({client_key: 'my client key', credentials: "my credentials"})
       client.client_key.wont_be_nil
@@ -12,19 +17,18 @@ describe AppFigures::Client do
     end
 
     it 'defualts to env vars if no block or options hash given' do
-      ENV['client_key'] = 'abcd'
-      ENV['credentials'] = '1234'
 
       client = AppFigures::Client.new()
 
       client.client_key.wont_be_nil
       client.credentials.wont_be_nil
 
-      ENV.delete('client_key')
-      ENV.delete('credentials')
     end
 
     it 'throws error if credentials or client key is nil' do
+      ENV.delete('client_key')
+      ENV.delete('credentials')
+
       Proc.new{AppFigures::Client.new()}.must_raise ArgumentError
     end
   end
