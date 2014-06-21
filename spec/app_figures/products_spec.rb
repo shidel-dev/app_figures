bce45acd050100a533314e560eaa250915c973f6
Shidel <joe@mytastebud.com>
2014-06-21 11:55:50 -0500

    adds error tests and uses Response module

--- spec/app_figures/products_spec.rb
+++ spec/app_figures/products_spec.rb
@@ -30,6 +30,7 @@ describe AppFigures::Client do
           'Authorization' => "Basic #{@client.credentials}"
         }
       )
+      .returns(Response.ok)
 
       @client.list_products
     end
@@ -43,8 +44,37 @@ describe AppFigures::Client do
           'Authorization' => "Basic #{@client.credentials}"
         }
       )
-
+      .returns(Response.ok)
       @client.list_products('apple')
+    end
+
+
+    it 'throws a not found error for a 404 code' do
+      @client.class.expects(:get)
+      .with('/products/mine',
+        query: {store: 'google_play'},
+        headers: {
+          'X-Client-Key' => @client.client_key,
+          'Authorization' => "Basic #{@client.credentials}"
+        }
+      )
+      .returns(Response.not_found)
+
+       Proc.new{ @client.list_products('google_play') }.must_raise AppFigures::Errors::NotFound
+    end
+
+    it 'throws a bad request error for a 500 to 600 code' do
+      @client.class.expects(:get)
+      .with('/products/mine',
+        query: {store: 'amazon'},
+        headers: {
+          'X-Client-Key' => @client.client_key,
+          'Authorization' => "Basic #{@client.credentials}"
+        }
+      )
+      .returns(Response.bad)
+
+       Proc.new{ @client.list_products('amazon') }.must_raise AppFigures::Errors::BadRequest
     end
   end
 end
